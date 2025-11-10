c93f4645c062d0b12813337a7b90a7ab90ecd16a
gheijer <g.a.den.heijer@hr.nl>
2025-11-10 08:19:30 +0000

    Remove Tuple and ValueTuple example methods
    Removed unused Tuple and ValueTuple examples from Program.cs.

--- Week10/W10_2/Material/Program.cs
+++ Week10/W10_2/Material/Program.cs
@@ -7,65 +7,6 @@ static class Program
         ArraysAreReferenceTypes();
     }
 
-    static void Tuples()
-    {
-        // Long way to create a Tuple
-        Tuple<int, string> longWay = new Tuple<int, string>(43, "hiu");
-
-        //Shorter way
-        //Tuple<type1, type2, ...> name = new(value1, value2, ...);
-        Tuple<string, int> person = new("Bob", 22);
-        Tuple<double, bool, string> examResults = new(2.2, false, "fail");
-
-        // Each item is accessed using Item1, Item2, Item3, ...
-        System.Console.WriteLine($"Name: {person.Item1}, age: {person.Item2}");
-
-        // person.Item2++; // Error! cannot change the values
-
-        // One of the most common use cases of tuples is as a method return type.
-        var isAdult = CheckIfAdultTuple(person.Item2);
-        System.Console.WriteLine($"Is adult? {isAdult.Item1}, they are a {isAdult.Item2}");
-    }
-
-    // checkIfAdultTuple(age) return bool (>=18) and string (adult or child)
-    public static Tuple<bool, string> CheckIfAdultTuple(int age)
-    {
-        bool isAdult = age >= 18;
-        string isAdultString = isAdult ? "Adult" : "Child";
-        return new(isAdult, isAdultString);
-    }
-
-    // ValueTuples: More versatile and we can change the values
-    // Note: Value tuples are passed by value
-    public static void ValueTuples()
-    {
-        //(type1 Name1, type2 Name2, ...) name = (value1, value2, ...);
-        (string Name, int Age) person = new("Sarah", 33);
-
-        // Each item is accessed using the given name
-        System.Console.WriteLine($"Name: {person.Name}, age: {person.Age}");
-
-        // We can change the values
-        person.Age++;
-
-        // checkIfAdult2
-        var check = CheckIfAdultValueTuple(12);
-
-        // we can rename item names returned by the method
-
-        (bool IsAdult, string Description) isAdult2 = CheckIfAdultValueTuple(person.Age);
-
-        System.Console.WriteLine($"Is adult? {isAdult2.IsAdult}, they are a {isAdult2.Description}");
-    }
-
-    // checkIfAdultValueTuple return bool (>=18) and string (adult or child)
-    public static (bool IsAdult, string IsAdultString) CheckIfAdultValueTuple(int age)
-    {
-        bool isAdult = age > 18;
-        string isAdultString = isAdult ? "Adult" : "Child";
-        return (isAdult, isAdultString);
-    }
-
     /*
         List vs array
         Memory:     List takes up more memory than an array 
