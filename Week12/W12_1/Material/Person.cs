5db29c941a2c80ff719bfa6b06399f22f8ea8f9e
gheijer <g.a.den.heijer@hr.nl>
2025-11-19 07:58:26 +0000

    Restored

---
+++ Week12/W12_1/Material/Person.cs
@@ -0,0 +1,13 @@
+﻿class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public override string ToString() => $"Name: {Name}, Age: {Age}";
+}
